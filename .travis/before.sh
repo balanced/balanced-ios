d7f64904e60b1faa0f5c1e1c210912fdb187489e
Ben Mills <ben@unfiniti.com>
2013-05-16 19:27:18 -0600

    Try git read-only url for cloning

--- .travis/before.sh
+++ .travis/before.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-cat > ~/.ssh/config <<'EOF'
-Host github.com
-  StrictHostKeyChecking no
-EOF
-
 brew update
 brew install xctool
-git clone git@github.com:kstenerud/iOS-Universal-Framework.git && pwd && cd iOS-Universal-Framework/Real\ Framework  && ls -la
+git clone git://github.com/kstenerud/iOS-Universal-Framework.git && pwd && cd iOS-Universal-Framework/Real\ Framework  && ls -la
 printf "\ny" | ./install.sh
