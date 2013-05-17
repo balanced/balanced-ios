bacc1b36e727736802b431c127461e4ba58f7d82
Ben Mills <ben@unfiniti.com>
2013-05-16 18:46:31 -0600

    Say yes to continue connecting for git clone

--- .travis/before.sh
+++ .travis/before.sh
@@ -3,5 +3,5 @@ set -e
 
 brew update
 brew install xctool
-git clone git@github.com:kstenerud/iOS-Universal-Framework.git && pwd && cd iOS-Universal-Framework/Real\ Framework  && ls -la
+printf "yes" | git clone git@github.com:kstenerud/iOS-Universal-Framework.git && pwd && cd iOS-Universal-Framework/Real\ Framework  && ls -la
 printf "\ny" | ./install.sh
